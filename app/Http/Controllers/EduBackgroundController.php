160d608f70689adaaac591ac8916badd227719cf
rubenson <rubenson.cristian@gmail.com>
2019-01-24 16:26:39 +0700

    persiapan awal

---
+++ app/Http/Controllers/EduBackgroundController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\EduBackground;
+use Illuminate\Http\Request;
+
+class EduBackgroundController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\EduBackground  $eduBackground
+     * @return \Illuminate\Http\Response
+     */
+    public function show(EduBackground $eduBackground)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\EduBackground  $eduBackground
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(EduBackground $eduBackground)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\EduBackground  $eduBackground
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, EduBackground $eduBackground)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\EduBackground  $eduBackground
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(EduBackground $eduBackground)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/WorkExpController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\WorkExp;
+use Illuminate\Http\Request;
+
+class WorkExpController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\WorkExp  $workExp
+     * @return \Illuminate\Http\Response
+     */
+    public function show(WorkExp $workExp)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\WorkExp  $workExp
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(WorkExp $workExp)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\WorkExp  $workExp
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, WorkExp $workExp)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\WorkExp  $workExp
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(WorkExp $workExp)
+    {
+        //
+    }
+}
